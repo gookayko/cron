b852c38c9685c4346ebb0795e2e6589267fd9a31
gookayko <wancz_520549@163.com>
2021-02-15 22:20:27 +0800

    增加*的自动，自动回复

--- src/main/java/com/xing/libao/cron/JoinInCL.java
+++ src/main/java/com/xing/libao/cron/JoinInCL.java
@@ -11,6 +11,8 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +23,7 @@ import java.util.List;
  * Time: 10:53
  */
 public class JoinInCL {
-    private static String code = "a695eX609d56e33b";
+    private String code = "a695eX60*d56*e33b";
     private static String postUrl = "http://cl.bearhk.info/register.php";
 
     public static void main(String[] args) {
@@ -29,20 +31,44 @@ public class JoinInCL {
         joinInCL.task();
     }
 
-    private void task(){
-        List<NameValuePair> list = new ArrayList<NameValuePair>();
-        list.add(new BasicNameValuePair("action","reginvcodeck"));
-        list.add(new BasicNameValuePair("reginvcode",code));
-        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list, Consts.UTF_8);
-        HttpPost httppost = new HttpPost(postUrl);
-        httppost.setEntity(entity);
-        CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
-        try {
-            CloseableHttpResponse response = closeableHttpClient.execute(httppost);
-            String result = EntityUtils.toString(response.getEntity(),"gb2312");
-            System.out.println(result);
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void task() {
+        String[] codes = code.split("\\*");
+        if (codes.length == 2) {
+            for (int i = 0; i <= 9; i++) {
+                List<NameValuePair> list = new ArrayList<NameValuePair>();
+                list.add(new BasicNameValuePair("action", "reginvcodeck"));
+                list.add(new BasicNameValuePair("reginvcode", codes[0] + i + codes[1]));
+                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list, Consts.UTF_8);
+                HttpPost httppost = new HttpPost(postUrl);
+                httppost.setEntity(entity);
+                CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+                try {
+                    CloseableHttpResponse response = closeableHttpClient.execute(httppost);
+                    String result = EntityUtils.toString(response.getEntity(), "gb2312");
+                    System.out.println(result + " - code: " + codes[0] + i + codes[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else if (codes.length == 3) {
+            for (int i = 0; i <= 9; i++) {
+                for (int j = 0; j <= 9; j++) {
+                    List<NameValuePair> list = new ArrayList<NameValuePair>();
+                    list.add(new BasicNameValuePair("action", "reginvcodeck"));
+                    list.add(new BasicNameValuePair("reginvcode", codes[0] + i + codes[1] + j + codes[2]));
+                    UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list, Consts.UTF_8);
+                    HttpPost httppost = new HttpPost(postUrl);
+                    httppost.setEntity(entity);
+                    CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+                    try {
+                        CloseableHttpResponse response = closeableHttpClient.execute(httppost);
+                        String result = EntityUtils.toString(response.getEntity(), "gb2312");
+                        System.out.println(result + " - code: " + codes[0] + i + codes[1] + j + codes[2]);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
 
     }
---
+++ src/main/java/com/xing/libao/cron/PostData.java
@@ -0,0 +1,77 @@
+package com.xing.libao.cron;
+
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.Consts;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Created by Administrator on 2015/6/3 0003.
+ */
+public class PostData {
+    private Set<String> strings = new HashSet<String>();
+
+    public static void main(String[] args) {
+        PostData postData = new PostData();
+        postData.task();
+    }
+
+    private void task() {
+        System.out.println("let's begin - " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        Document document = null;
+        try {
+            document = Jsoup.connect("http://cl.bearhk.info/thread0806.php?fid=7&page=2").header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36").header("Host", "cl.bearhk.info").get();
+            Elements elements = document.select("tr.tr3.t_one");
+            for (Element element : elements) {
+                String title = element.child(1).child(0).child(0).text();
+                String url = element.child(1).child(0).child(0).attr("abs:href");
+                String sTid = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf(".html"));
+                int tid = 0;
+                if (StringUtils.isNotEmpty(sTid) && StringUtils.isNumeric(sTid)) {
+                    tid = Integer.parseInt(sTid);
+                }
+                System.out.println(url + " --- " + title);
+                if (!strings.contains(url) && tid > 0) {
+                    List<NameValuePair> list = new ArrayList<NameValuePair>();
+                    list.add(new BasicNameValuePair("atc_usesign", "1"));
+                    list.add(new BasicNameValuePair("atc_convert", "1"));
+                    list.add(new BasicNameValuePair("atc_autourl", "1"));
+                    list.add(new BasicNameValuePair("atc_title", URLEncoder.encode("Re:" + title, "gb2312")));
+                    list.add(new BasicNameValuePair("atc_content", "1024"));
+                    list.add(new BasicNameValuePair("step", "2"));
+                    list.add(new BasicNameValuePair("action", "reply"));
+                    list.add(new BasicNameValuePair("fid", "7"));
+                    list.add(new BasicNameValuePair("atc_attachment", "none"));
+                    list.add(new BasicNameValuePair("tid", tid + ""));
+                    UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list, Consts.UTF_8);
+                    HttpPost httppost = new HttpPost(url);
+                    httppost.setEntity(entity);
+                    CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+                    CloseableHttpResponse response = closeableHttpClient.execute(httppost);
+                    String content = EntityUtils.toString(response.getEntity());
+                    System.out.println(content);
+                    strings.add(url);
+                }
+                Thread.sleep(1025000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
